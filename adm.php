3f81e015137a8c1e9649d0dbb50b501073252487
zinhoflag1 <zinhoflag1@gmail.com>
2023-04-14 16:20:15 -0300

    14.04.2023

---
+++ adm.php
@@ -0,0 +1,27 @@
+<?php
+?>
+
+<label>Colunas</label>
+<select name="selpart" id="selpart">
+    <option value="12">12 Colunas</option>
+    <option value="8">8 Colunas</option>
+    <option value="6">6 Colunas</option>
+    <option value="4">4 Colunas</option>
+    <option value="3">3 Colunas</option>
+    <option value="2">2 Colunas</option>
+</select>
+
+<label for="">Tipo</label>
+<select name="selpart" id="selpart">
+    <option value="">Cabeçalho</option>
+    <option value="">Banners</option>
+    <option value="">Noticias</option>
+    <option value="">Noticias Destaque</option>
+    <option value="">Mini-banner</option>
+    <option value="">Informativos</option>
+    <option value="">Rodapé</option>
+</select>
+
+
+
+
